53902e13cf2ea05aa8c89747f15ba0fd30a12c30
InnocentOmenka <innocent.omenka@decagon.dev>
2023-04-27 17:10:48 +0100

    API gateway

--- employee-service/src/main/java/com/innocodes/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/innocodes/employeeservice/service/APIClient.java
@@ -2,13 +2,14 @@ package com.innocodes.employeeservice.service;
 
 import com.innocodes.employeeservice.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") @RequestBody String departmentCode);
